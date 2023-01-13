4e2b5a70b48bcaf9873ce14c132e07c59600c5ce
Richard Oropesa <richardoro2001@gmail.com>
2023-01-13 16:15:59 -0500

    fix re-render and warnings

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,46 +1,47 @@
 import axios from 'axios'
-import React, {useEffect, useState} from 'react'
-import { Film } from '../components/Film';
+import {useEffect, useState, useCallback} from 'react'
+import  Film  from '../components/Film';
 import { Img } from '../Types';
 import { FilmInfo } from '../components/FilmInfo';
 
 export const Home = () => {
     const [infoVisible, setInfoVisible] = useState<boolean>(false);
     const [current, setCurrent] = useState<Img>({url:"",title:""});
-    const showInfo = (info: Img) =>{
+    const showInfo = useCallback((info: Img) =>{
        setCurrent(info);
        setInfoVisible(true);
-    }
+    },[])
     const hideInfo = () =>{
         setInfoVisible(false);
     }
     const [imgs, setImgs] = useState<Array<Img>>([]);
-
+    const addImg = useCallback((img:Img)=>{
+        setImgs((previus)=>[...previus,img]);
+        return img;
+    },[])
     useEffect(() => {
         axios.get(process.env.REACT_APP_API_URL+'discover/movie/?&api_key='+process.env.REACT_APP_API_KEY)
         .then((res=>{
             res.data.results.map((film:any)=>{
-                setImgs((previus)=>[...previus,{
+                let newImg: Img = {
                     url: film.poster_path, 
                     title: film.original_title,
                     genres: film.genres,
                     overview: film.overview,
                     popularity: film.popularity,
                     release_date: film.release_date,
-                }])
-                console.log(film)
+                }
+                return addImg(newImg);  
             });
-            console.log(imgs)
         }))
         
-    }, [])
+    }, [addImg])
     
     
   return (
     <>
         <div className='flex flex-row flex-wrap gap-4 justify-center p-4'>
-            {imgs.map((film:Img,index:number)=><Film key={index} info={film} handleSubmit={(info:Img)=>showInfo(info)}/>
-            )}
+            {imgs.map((film:Img,index:number)=><Film key={index} info={film} handleSubmit={showInfo}/>)}
         </div>
         {infoVisible && <FilmInfo hideInfo={hideInfo} info={current}/>}
         
