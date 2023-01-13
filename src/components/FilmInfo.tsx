702b6731543a5d649e0dd76c41adc656ccd9c7bd
Richard Oropesa <richardoro2001@gmail.com>
2023-01-13 11:12:02 -0500

    fix: film info

--- src/components/FilmInfo.tsx
+++ src/components/FilmInfo.tsx
@@ -12,8 +12,8 @@ export const FilmInfo:React.FC<Props> = (props) => {
         props.hideInfo();
     }
   return (
-    <div className='flex justify-center fixed w-full top-0 bg-slate-900 bg-opacity-75 h-full font-serif' onClick={handleClick}>
-        <div className='absolute w-1/2 h-[600px] bg-slate-700 top-24  p-7 flex flex-row gap-3 rounded-lg'>
+    <div className='flex justify-center items-center fixed w-full top-0 bg-slate-900 bg-opacity-75 h-full font-serif' onClick={handleClick}>
+        <div className='w-1/2 overflow-y-auto max-h-[80vh] bg-slate-700 top-24  p-7 flex flex-row gap-3 rounded-lg'>
             <div className='flex flex-col gap-3 items-center text-2xl w-full'>
                 <img alt={props.info.title} src={"https://www.themoviedb.org/t/p/w220_and_h330_face/"+props.info.url} className='w-80'/>
                 <div>{props.info.title}</div>
