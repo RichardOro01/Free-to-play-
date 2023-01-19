623e87c6365e4449b10dd944f7dc2d72bf870d1f
Richard Oropesa <93955101+RichardOro01@users.noreply.github.com>
2023-01-18 19:43:15 -0500

    fix: min-height-screen

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -4,7 +4,7 @@ import { Header } from '../components/Header';
 
 export const Layout = () => {
   return (
-    <div id="app-content">
+    <div id="app-content" className='min-h-screen'>
       <Header/>
       <div id='body-container' className=''>
         <Outlet/>
